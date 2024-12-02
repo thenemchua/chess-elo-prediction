9d2b1812bb90cd705c6f105cea8751f1fd523ba6
alain lim <alain.lim@edu.esiee.fr>
2024-12-02 18:04:02 +0100

    fonction create matrice from pgn modifiée pour que n=1 donne qu'une matrice et fonction pour créer sparse matrix en developpement

--- Utils/matrix_creation.py
+++ Utils/matrix_creation.py
@@ -2,6 +2,8 @@ import chess
 import chess.pgn
 import pandas as pd
 import numpy as np
+from scipy.sparse import lil_matrix 
+from scipy.sparse import csr_matrix
 
 
 def list_epd_per_move(pgn):
@@ -72,15 +74,16 @@ def create_matrice_from_pgn(pgn,n):
     list_matrices=[]
 
     for epd in list_epd:
-        matrice= np.zeros((n,8,8), dtype=bool)
+        matrice= np.zeros((n,8,8), dtype=np.int8)
         if n==1:
+            matrice= np.zeros((8,8), dtype=np.int8)
             for i, row in enumerate(epd):
                 k=0
                 for j, char in enumerate(row):
                     if char.isdigit():
                         k+=int(char)-1
                     else:
-                        matrice[0,i,j+k]=weights[char][0]
+                        matrice[i,j+k]=weights[char][0]
             list_matrices.append(matrice)
         elif n==2:
             for i, row in enumerate(epd):
@@ -109,4 +112,42 @@ def create_matrice_from_pgn(pgn,n):
         else:
             return "n doit être égale à 1, 2 ou 12"
 
+    return list_matrices
+
+
+def create_sparse_matrix_from_pgn(pgn):
+
+    """
+    """
+
+    list_epd= list_epd_per_move(pgn)
+
+    matrice_12={"R":0,
+        "N":1,
+        "B":2,
+        "Q":3,
+        "K":4,
+        "P":5,
+        "r":6,
+        "n":7,
+        "b":8,
+        "q":9,
+        "k":10,
+        "p":11
+        }
+
+    list_matrices=[]
+
+    for epd in list_epd:
+        matrice= np.zeros((12,8,8), dtype=bool)
+        for i, row in enumerate(epd):
+            k=0
+            for j, char in enumerate(row):
+                if char.isdigit():
+                    k+=int(char)-1
+                else:
+                    matrice[matrice_12[char],i,j+k]=True
+        matrice = [csr_matrix(mat) for mat in matrice]
+        list_matrices.append(matrice)
+    
     return list_matrices
