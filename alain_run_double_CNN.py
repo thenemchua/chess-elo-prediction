0f3a1b2b77a706c0e573e563aa68bea170adb456
alain lim <alain.lim@edu.esiee.fr>
2024-12-04 19:39:34 +0100

    modified rady to run

--- alain_run_double_CNN.py
+++ alain_run_double_CNN.py
@@ -10,8 +10,8 @@ bucket_name="chess_elo_prediction_lw1812"
 
 # Loading data from GCP
 print('Loading data from GCP...')
-pkl_df = utils.read_pickle_from_gcloud_df(bucket_name, gcloud_path="data/concat_result.pkl")
-full_df = utils.read_parquet_from_gcloud_df(bucket_name, gcloud_path="data/cleaned_full_blitz_50000.parquet")
+pkl_df = utils.read_pickle_from_gcloud_df(bucket_name, gcloud_path="preprocessed_pgn/concat_result.pkl")
+full_df = utils.read_parquet_from_gcloud_df(bucket_name, gcloud_path="pgn_time_increment_rating_data/cleaned_full_blitz_50000.parquet")
 print('\nData loaded !')
 
 print('\nCreating X and y...')
@@ -25,7 +25,7 @@ time = full_df.loc[0:406894].time_per_move
 time_pad = pad_sequences(time, padding='post', maxlen=150, dtype='float')
 X_pad = pad_sequences(X, padding='post',maxlen=150, dtype= "int8")
 
-y = full_df.loc[0:406894].white_rating
+y = full_df.loc[0:406894].black_rating
 print('\nX,y initialized!')
 
 
@@ -37,8 +37,8 @@ print('\nModel initialized!')
 
 
 print('\nTraining model...')
-dl_models.train_model(double_cnn, [X_pad, time_pad], y, ckp_filename='nicolas_double_CNN_on_white', epochs=27, validation_split=.2, patience=100)
-filepath = os.path.join('checkpoint', "nicolas_double_CNN_on_white.model.keras")
-utils.upload_parquet_to_gcp(bucket_name, filepath, 'models/nicolas_double_CNN.model.keras')
+dl_models.train_model(double_cnn, [X_pad, time_pad], y, ckp_filename='alain_double_CNN_on_black', epochs=27, validation_split=.2, patience=100)
+filepath = os.path.join('checkpoint', "alain_double_CNN_on_black.model.keras")
+utils.upload_parquet_to_gcp(bucket_name, filepath, 'models/alain_double_CNN.model.keras')
 
 print('\nEverything done!')
